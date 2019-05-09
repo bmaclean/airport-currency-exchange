0adac2f931d75b9d75df158538007e4a464ae6b2
Brendan M <brendan.maclean94@gmail.com>
2019-05-09 17:07:05 -0600

    Actions for fetching and successfully retrieving currency data.

--- src/app/store/actions/index.js
+++ src/app/store/actions/index.js
@@ -11,11 +11,19 @@ export const SELL_CURRENCY = 'SELL_CURRENCY';
 // Update admin settings
 export const UPDATE_SETTINGS = 'UPDATE_SETTINGS';
 
+// fetch and update currency request
+export const FETCH_CURRENCIES = 'FETCH_CURRENCIES';
+
+// currency fetch and update failed
+export const FETCH_CURRENCIES_FAILURE = 'FETCH_CURRENCIES_FAILURE';
+
+// receive currency data
+export const RECEIVE_CURRENCIES = 'RECEIVE_CURRENCIES';
+
 /**
  * ACTION CREATORS
  */
 
-// TODO: margins/rates?
 export function buyCurrency(currency, amount) {
 	return {type: BUY_CURRENCY, currency, amount};
 }
@@ -26,4 +34,38 @@ export function sellCurrency(currency, amount) {
 
 export function updateSettings(updatedSettings) {
 	return {type: UPDATE_SETTINGS, updatedSettings};
+}
+
+export function receiveCurrencies(currencyData) {
+	return {
+		type: RECEIVE_CURRENCIES,
+		rates: currencyData.rates,
+		receivedAt: Date.now()
+	};
+}
+
+export function fetchCurrenciesFailed() {
+	return {
+		type: FETCH_CURRENCIES_FAILURE,
+		receivedAt: Date.now()
+	};
+}
+
+export function fetchCurrencies(baseCurrency, currencies) {
+	return function(dispatch) {
+		return fetch(
+			`https://api.exchangeratesapi.io/latest?base=${baseCurrency}&symbols=${currencies.join(
+				','
+			)}`
+		)
+			.then(
+				response => response.json(),
+				error => {
+					// eslint-disable-next-line no-console
+					console.error('An error occurred.', error);
+					dispatch(fetchCurrenciesFailed());
+				}
+			)
+			.then(data => dispatch(receiveCurrencies(data)));
+	};
 }
