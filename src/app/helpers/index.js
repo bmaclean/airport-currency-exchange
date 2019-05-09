1982100e73a4415d1a06161758c132d9b57ddce8
Brendan M <brendan.maclean94@gmail.com>
2019-05-09 17:13:14 -0600

    toDecimalPlace helper for displaying numerical data with varying decimal places.

---
+++ src/app/helpers/index.js
@@ -0,0 +1,1 @@
+export {default as toDecimalPlace} from './toDecimalPlace';
---
+++ src/app/helpers/toDecimalPlace.js
@@ -0,0 +1,5 @@
+const toDecimalPlace = (num, decimals) => {
+	return parseFloat(num.toFixed(decimals));
+};
+
+export default toDecimalPlace;
