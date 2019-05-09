dff5041ddf6f2bd266dafc48c4af82f28b46c606
Brendan M <brendan.maclean94@gmail.com>
2019-05-09 17:09:42 -0600

    Add middleware to redux store for asynchronous actions, logging.

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,6 +1,18 @@
-import {createStore} from 'redux';
-import {default as acxApp} from './reducers';
+import {applyMiddleware, createStore} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import {createLogger} from 'redux-logger';
+import {default as acxAppReducer} from './reducers';
 
-const store = createStore(acxApp);
+const loggerMiddleware = createLogger();
+
+function configureStore(preloadedState) {
+	return createStore(
+		acxAppReducer,
+		preloadedState,
+		applyMiddleware(thunkMiddleware, loggerMiddleware)
+	);
+}
+
+const store = configureStore();
 
 export default store;
