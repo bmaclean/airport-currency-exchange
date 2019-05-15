64c70d2ab138603d4a6aacd478dd85b03ba46d49
Brendan M <brendan.maclean94@gmail.com>
2019-05-15 16:59:21 -0600

    Removes redux logger.

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,15 +1,12 @@
 import {applyMiddleware, createStore} from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import {createLogger} from 'redux-logger';
 import {default as acxAppReducer} from './reducers';
-
-const loggerMiddleware = createLogger();
 
 function configureStore(preloadedState) {
 	return createStore(
 		acxAppReducer,
 		preloadedState,
-		applyMiddleware(thunkMiddleware, loggerMiddleware)
+		applyMiddleware(thunkMiddleware)
 	);
 }
 
